17445cbf6f91775f6aa81296a06ca346a5bfbb6a
MaouAqim <maouaqim@gmail.com>
2024-12-12 16:21:37 +0700

    production

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://lemandu-production.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
