7b6954601a26993bbc9121f8f8bd5a7c21e2620f
MaouAqim <maouaqim@gmail.com>
2024-12-12 17:27:22 +0700

    production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
@@ -22,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
         Vite::prefetch(concurrency: 3);
         Inertia::share([
             'auth' => function () {
